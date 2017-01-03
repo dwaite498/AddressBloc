40ff5bfa4af3833606b35ff5ad3653a26abf8ff6
David Waite <dwaite498@gmail.com>
2017-01-03 19:14:59 +0000

    create an entry

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -17,7 +17,61 @@ class MenuController
         print "Enter your selection"
         
         selection = gets.to_i
-        puts "You picked #{selection}"
+        
+        
+        case selection
+            when 1
+                system "clear"
+                view_all_entries
+                main_menu
+            when 2
+                system "clear"
+                create_entry
+                main_menu
+            when 3
+                system "clear"
+                search_entries
+                main_menu
+            when 4
+                system "clear"
+                read_CSV
+                main_menu
+            when 5
+                system "clear"
+                puts "Good bye"
+                exit(0)
+                
+            else 
+                system "clear"
+                puts "Sorry, that is not a valid input. Please try again."
+                main_menu
+        end
+    end
+    
+    def view_all_entries
+        system "clear"
+        puts "New AddressBloc Entry"
+        
+        print "Name: "
+        name = gets.chomp
+        print "Phone number: "
+        phone = gets.chomp
+        print "Email: "
+        email = gets.chomp
+        
+        address_book.add_entry(name, phone, email)
+        
+        system "clear"
+        puts "New entry created"
+    end
+    
+    def create_entry
+    end
+    
+    def search_entries
+    end
+    
+    def read_CSV
     end
 
 end
